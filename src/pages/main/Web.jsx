3b1ca367e00a546d403b475d7af123a454cbdde7
Qifei Cui <qifei@ucsb.edu>
2024-06-29 20:40:52 +0800

    Website 0.3.0

--- src/pages/main/Web.jsx
+++ src/pages/main/Web.jsx
@@ -3,28 +3,31 @@ import './web.css';
 import Sidebar from '../../components/sidebar/Sidebar.jsx';
 import Home from '../../components/home/Home.jsx';
 import About from '../../components/about/About.jsx';
-import Service from '../../components/services/Services.jsx';
+import Certificate from '../../components/certificates/Certificates.jsx';
 import Resume from '../../components/resume/Resume.jsx'; 
 import Portfolio from '../../components/portfolio/Portfolio.jsx';
 import Pricing from '../../components/pricing/Pricing.jsx';
 import Testimonials from '../../components/testimonials/Testimonials.jsx';
 import Blog from '../../components/blog/Blog.jsx';
 import Contact from '../../components/contact/Contact.jsx'
+import Footer from '../../components/footer/Footer.jsx';
 
 
 function Body() {
   return (
     <>
         <Sidebar />
-        <main className='main'>
+      <main className='main'>
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
         <Home />
         <About />
-        <Service />
+        <Certificate />
         <Resume />
-        <Portfolio />
         <Testimonials />
-        <Blog />
         <Contact />
+        <Footer />
       </main>
     </>
   )
